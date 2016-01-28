cf6bfadbbb9a8ec5c73e62a3a20d5daad33de272
Jose Almeida <zectorpt@JoseAlmeidaLAP.ocado.com>
2016-01-28 08:55:59 +0000

    Optimization to get URLs

--- mailsearch.sh
+++ mailsearch.sh
@@ -12,15 +12,17 @@ searchterm=$(echo $searchterm | sed -e 's/\ /+/g')
 echo -e '\nExtraindo URLs\n'
 lynx -dump "http://www.google.co.uk/search?q=$searchterm&num=9999" > search
 sleep 1
-cat search |awk '{print $1}'|grep www > UrlCleanList
-rm -f search
+cat search | grep -Eo '(http|https)://[^/"]+' > UrlCleanList
+cat UrlCleanList |sort| uniq > UrlCleanListUniq
 
 while read -r line
   do
      curl -s "$line" >> UrlLevelOne
 #    lynx -dump $line >> site
-  done < UrlCleanList
+  done < UrlCleanListUniq
+
 echo -e '\nA extrair mails'
+
 sleep 2
 grep -E -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b" UrlLevelOne > Mails
 sleep 1

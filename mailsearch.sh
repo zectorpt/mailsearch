d416bf18fdef5a8865dfd6ba0bce88e3409f5498
root <root@JoseAlmeidaLAP.ocado.com>
2016-01-28 12:46:09 +0000

    Final release 0.9

--- mailsearch.sh
+++ mailsearch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#######################################
+# josemedeirosdealmeida@gmail.com     #
+# josemedeirosdealmeida.com           #
+#A good help to find mails to SPAM    #
+#                                     #
+#Usage:                               #
+# ./mailsearch.sh                     #
+#######################################
 
 #Pede termo da pesquisa
 if [[ $(echo $*) ]]; then
@@ -16,11 +24,17 @@ sleep 1
 #grep the urls sorted and uniq to UrlCleanListUniq
 cat search | grep -Eo '(http|https)://[^/"]+' > UrlCleanList
 cat UrlCleanList |sort| uniq > UrlCleanListUniq
+sleep 2
+
+#Implement counter 1
+numberoflines=$(cat UrlCleanListUniq|wc -l)
 
 #Get all html of the sites to UrlLevelOne
 while read -r line
   do
      curl -s "$line" >> UrlLevelOne
+     echo "We need to process more $numberoflines lines"
+     ((numberoflines--))
   done < UrlCleanListUniq
 
 #grep all html of UrlLevelOne and export to UrlLevelTwo and after this, to UrlCleanListUniq
@@ -28,11 +42,16 @@ cat UrlLevelOne | grep -Eo '(http|https)://[^/"]+' > UrlLevelTwo
 sleep 1
 cat UrlLevelTwo |sort| uniq > UrlCleanListUniq
 
+#Implement counter 2
+numberoflines=$(cat UrlCleanListUniq|wc -l)
 
 #Get all html of the sites to Level2HTML
+echo -e "\nA exportar os $numberoflines resultados para Level2HTML"
 while read -r line
   do
      curl -s "$line" >> Level2HTML
+     echo "We need to process more $numberoflines lines"
+     ((numberoflines--))
   done < UrlCleanListUniq
 
 echo -e '\nA extrair mails'
@@ -44,10 +63,25 @@ sleep 1
 
 #Export the file Mails sorted and uniq to MailsFinalList
 cat Mails|sort|uniq > MailsFinalList
+echo -e "\nCleaning trash... png"
+sleep 1
+sed "/^[^ ]png\png/d" MailsFinalList > MailsFinalList1
+echo -e "\nCleaning trash... jpg"
+sleep 1
+sed "/^[^ ]jpg\jpg/d" MailsFinalList1 > MailsFinalList2
+echo -e "\nCleaning trash... gif"
+sleep 1
+sed "/^[^ ]gif\gif/d" MailsFinalList2 > MailsFinalList
+sleep 1
+echo -e "\nDeletemp temp files"
+sleep 1
 rm -f Mails
 rm -f Level2HTML
 rm -f UrlCleanListUniq
 rm -f UrlLevelTwo
 rm -f UrlLevelOne
 rm -f UrlCleanList
+rm -f MailsFinalList1
+rm -f MailsFinalList2
 rm -f search
+echo -e "\nDONE! Check you MailsFinalList file\n"

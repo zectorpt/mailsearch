70fc38dd923d63b804debaa745d5056cb1f9298e
root <root@JoseAlmeidaLAP.ocado.com>
2016-02-01 07:07:32 +0000

    Final release Teste com dois processos

--- helper1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-numberoflines=454
-while read -r line
-  do
-     curl -s "" >> Level2HTML
-     echo "We need to process more  lines"
-     ((numberoflines--))
-  done < xaa
--- helper2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-numberoflines=453
-while read -r line
-  do
-     curl -s "" >> Level2HTML
-     echo "We need to process more  lines"
-     ((numberoflines--))
-  done < xab
